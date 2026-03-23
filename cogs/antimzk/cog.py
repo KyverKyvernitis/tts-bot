80f2dbfa1e458aa5b06cc8334b4fbeb499f15c65
KyverKyvernitis <58307221+KyverKyvernitis@users.noreply.github.com>
2026-03-22 22:29:46 -0300

    Delete cogs/antimzk directory

--- cogs/antimzk/cog.py
+++
@@ -1,9 +0,0 @@
-from .base import AntiMzkBase
-from .focus import AntiMzkFocusMixin
-from .poker import AntiMzkPokerMixin
-from .slash_commands import AntiMzkCommandMixin
-from .triggers import AntiMzkTriggerMixin
-
-
-class AntiMzkCore(AntiMzkCommandMixin, AntiMzkFocusMixin, AntiMzkPokerMixin, AntiMzkTriggerMixin, AntiMzkBase):
-    pass
