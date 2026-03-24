b3c40b2d0d587770be484dcb5ba17c638369b87e
KyverKyvernitis <58307221+KyverKyvernitis@users.noreply.github.com>
2026-03-24 00:25:25 -0300

    Add files via upload

--- cogs/gincana/constants.py
+++ cogs/gincana/constants.py
@@ -3,10 +3,6 @@ import re
 import discord
 from discord import app_commands
 
-from config import GUILD_IDS
-
-
-_GUILD_OBJECTS = [discord.Object(id=guild_id) for guild_id in GUILD_IDS]
 _FOCUS_WORD_RE = re.compile(r"(?<!\w)focus(?!\w)", re.IGNORECASE)
 _ROLE_TOGGLE_WORD_RE = re.compile(r"^\s*pica\s*$", re.IGNORECASE)
 _DJ_TOGGLE_WORD_RE = re.compile(r"^\s*dj\s*$", re.IGNORECASE)
@@ -22,7 +18,7 @@ _DJ_DURATION_SECONDS = 6 * 60 * 60
 
 
 def _guild_scoped():
-    return app_commands.guilds(*_GUILD_OBJECTS) if _GUILD_OBJECTS else (lambda f: f)
+    return lambda f: f
 
 
 CHIPS_INITIAL = 200
