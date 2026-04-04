c14b99c70694d2021e06dd73586b60fb3d15acb1
KyverKyvernitis <dilma9823@gmail.com>
2026-04-04 16:29:42 -0300

    Fix sinuca activity startup scripts

--- start-sinuca-server.sh
+++ start-sinuca-server.sh
@@ -3,6 +3,7 @@ set -u
 
 SERVER_DIR="/home/ubuntu/bot/activity /sinuca-server"
 ENV_FILE="$SERVER_DIR/.env"
+NODE_BIN="/home/ubuntu/.nvm/versions/node/v20.20.2/bin/node"
 
 cd "$SERVER_DIR" || exit 1
 
@@ -12,4 +13,4 @@ if [ -f "$ENV_FILE" ]; then
   set +a
 fi
 
-exec /usr/bin/node dist/index.js
+exec "$NODE_BIN" dist/index.js
